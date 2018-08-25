b5f2c6ed3595d974ce77609bca128563fa70d775
Andrew Hoetker <ahoetker@me.com>
2018-08-25 03:47:15 -0700

    Add autoreport script and entrypoint

---
+++ matlabreport/scripts/autoreport.py
@@ -0,0 +1,32 @@
+# matlabreport.py - Click interface to publish MATLAB reports
+
+import click
+import os
+from matlabreport.makereport import makeReport
+from matlabreport.publishmfile import publishMfile
+from matlabreport.openreport import openReport
+
+
+@click.command()
+@click.option("--pubformat", default="html")
+@click.option("--funcs", is_flag=True, help="Append user functions.")
+@click.option("-p", is_flag=True, help="Publish report after building.")
+@click.option("-t", is_flag=True, help="Use pdflatex to typeset report.")
+@click.option("-v", is_flag=True, help="Verbose pdflatex console output.")
+@click.option("-s", is_flag=True, help="Show published report.")
+def autoreport(pubformat, funcs, p, t, v, s):
+    """
+    Make a MATLAB report m-file, and optionally publish it.
+    """
+    outline = os.path.join(".", "outline.yml")
+    report_m = os.path.join(".", "report.m")
+    makeReport(outline, report_m, funcs, auto=True)
+
+    if p is True:
+        published_report = publishMfile(report_m, pubformat, t, v)
+
+        if s is True:
+            openReport(published_report)
+
+    os.remove(outline)
+    os.remove(report_m)
--- tests/test_makereport.py
+++ tests/test_makereport.py
@@ -16,14 +16,14 @@ def test_makereport():
     os.chdir(resources)
 
     # Test report generation with no functions included
-    makeReport(outline_file, report, funcs=False)
+    makeReport(outline_file, report, funcs=False, auto=False)
     with open(report, "r") as f1:
         with open(os.path.join(resources, "dummyreport_nofuncs.m"), "r") as f2:
             assert f1.read() == f2.read()
     os.remove(report)
 
     # Test report generation WITH functions
-    makeReport(outline_file, report, funcs=True)
+    makeReport(outline_file, report, funcs=True, auto=False)
     with open(report, "r") as f1:
         with open(os.path.join(resources, "dummyreport_funcs.m"), "r") as f2:
             assert f1.read() == f2.read()
