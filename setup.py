3efabf0beffd18b77f03b9173cfaf60c6ce83fff
ahoetker <ahoetker@me.com>
2018-09-06 00:58:50 -0700

    version bump to 0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="matlabreport",
-    version="0.5.0",
+    version="0.6.0",
     description="Make and publish MATLAB reports.",
     url="https://github.com/ahoetker/matlabreport",
     author="Andrew Hoetker",
